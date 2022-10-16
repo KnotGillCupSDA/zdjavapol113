af35d29e6a260e40e6da2870b2aaa39c36c3e630
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2022-10-16 10:52:46 +0200

    - PalindromeChecker csvSource

--- src/test/java/com/sda/testingadvanced/parametrized/PalindromeCheckerTest.java
+++ src/test/java/com/sda/testingadvanced/parametrized/PalindromeCheckerTest.java
@@ -1,14 +1,17 @@
 package com.sda.testingadvanced.parametrized;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class PalindromeCheckerTest {
 
 	@ParameterizedTest
-	@ValueSource(strings = {"", "TeT", "Tet", "Te T"})
+	@ValueSource(strings = { "", "TeT", "Tet", "Te T" })
 	void shouldBePalindrome(String text) {
 
 		//when
@@ -19,7 +22,7 @@ class PalindromeCheckerTest {
 	}
 
 	@ParameterizedTest
-	@ValueSource(strings = {"abc"})
+	@ValueSource(strings = { "abc" })
 	void shouldNotBePalindrome(String text) {
 
 		//when
@@ -27,5 +30,11 @@ class PalindromeCheckerTest {
 
 		//then
 		assertFalse(actual);
+	}
+
+	@ParameterizedTest
+	@CsvSource(value = { "TeT, true", "Tet, true", "Te T, true", "abc, false" })
+	void testIsPalindrome(String text, boolean expected) {
+		assertEquals(expected, PalindromeChecker.isPalindrome(text));
 	}
 }
