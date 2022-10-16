99687b44d7358c7134a3ffbed3cffd4fd3882139
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2022-10-16 10:00:18 +0200

    - DivisibilityChecker simple test

--- src/test/java/com/sda/testingadvanced/parametrized/DivisibilityCheckerTest.java
+++ src/test/java/com/sda/testingadvanced/parametrized/DivisibilityCheckerTest.java
@@ -2,6 +2,20 @@ package com.sda.testingadvanced.parametrized;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 class DivisibilityCheckerTest {
 
+	@Test
+	void shouldBeDivisibleBy3() {
+		//given
+		int number = 6;
+
+		//when
+		boolean actual = DivisibilityChecker.isDivisibleBy3(number);
+
+		//then
+		Assertions.assertTrue(actual);
+	}
 }
