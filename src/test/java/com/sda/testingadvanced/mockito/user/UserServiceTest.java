16e3ae463b62dc8a624cde11f8eba54075cf8bf5
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2022-10-16 15:49:51 +0200

    - UserServiceTest add test

--- src/test/java/com/sda/testingadvanced/mockito/user/UserServiceTest.java
+++ src/test/java/com/sda/testingadvanced/mockito/user/UserServiceTest.java
@@ -1,15 +1,15 @@
 package com.sda.testingadvanced.mockito.user;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 
 import java.util.NoSuchElementException;
 import java.util.Optional;
-import java.util.Random;
 
-import org.junit.jupiter.api.BeforeEach;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -45,5 +45,35 @@ class UserServiceTest {
 	@Test
 	void shouldThrowNoSuchElementExceptionWhenUserDoesNotExists() {
 		assertThrows(NoSuchElementException.class, () -> userService.getUserById(-1L));
+	}
+
+	@Test
+	void shouldAddValidUser() {
+		//given
+		final User user = new User(7L, "MockedName", "MockedLastName");
+		when(userValidator.isUserValid(user)).thenReturn(true);
+		when(userRepository.addUser(user)).thenReturn(user);
+
+		//when
+		final User addedUser = userService.addUser(user);
+
+		//then
+		assertNotNull(addedUser);
+		assertEquals(user, addedUser);
+	}
+
+	@Test
+	void shouldThrowsExceptionForInvalidUser() {
+		//given
+		final long id = 7L;
+		final User user = new User(id, "MockedName", "MockedLastName");
+		when(userValidator.isUserValid(user)).thenReturn(false);
+
+		//when, then
+		Assertions.assertThatExceptionOfType(IllegalArgumentException.class)
+				.isThrownBy(() -> userService.addUser(user))
+				.withMessageContaining(String.valueOf(id));
+
+		Mockito.verifyNoInteractions(userRepository);
 	}
 }
