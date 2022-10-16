f0e3e62cc51d91762d8ca5b1fed14f3445f02099
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2022-10-16 10:13:31 +0200

    - DivisibilityChecker simple parametrized test

--- src/test/java/com/sda/testingadvanced/parametrized/DivisibilityCheckerTest.java
+++ src/test/java/com/sda/testingadvanced/parametrized/DivisibilityCheckerTest.java
@@ -1,21 +1,34 @@
 package com.sda.testingadvanced.parametrized;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class DivisibilityCheckerTest {
 
-	@Test
-	void shouldBeDivisibleBy3() {
-		//given
-		int number = 6;
+	@ParameterizedTest
+	@ValueSource(ints = {6, 12, -3, 0, 82366794})
+	void shouldBeDivisibleBy3(int number) {
 
 		//when
 		boolean actual = DivisibilityChecker.isDivisibleBy3(number);
 
 		//then
-		Assertions.assertTrue(actual);
+		assertTrue(actual);
+	}
+
+	@Test
+	void shouldNotBeDivisibleBy3() {
+		//given
+		int number = 7;
+
+		//when
+		boolean actual = DivisibilityChecker.isDivisibleBy3(number);
+
+		//then
+		assertFalse(actual);
 	}
 }
