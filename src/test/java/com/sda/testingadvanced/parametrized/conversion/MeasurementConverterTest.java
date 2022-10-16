ca4443be1c755d5c5e060762a244d0d37aacbaa7
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2022-10-16 11:42:49 +0200

    - MeasurementConverter enum source example 2

--- src/test/java/com/sda/testingadvanced/parametrized/conversion/MeasurementConverterTest.java
+++ src/test/java/com/sda/testingadvanced/parametrized/conversion/MeasurementConverterTest.java
@@ -3,6 +3,8 @@ package com.sda.testingadvanced.parametrized.conversion;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.Random;
+
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
 
@@ -17,5 +19,15 @@ class MeasurementConverterTest {
 		assertTrue(actual < Double.MAX_VALUE);
 		assertTrue(actual > Double.MIN_VALUE);
 		assertNotEquals(actual, Double.NaN);
+	}
+
+	@ParameterizedTest
+	@EnumSource(value = ConversionType.class,
+	names = {"METERS_TO_YARDS", "INCHES_TO_CENTIMETERS", "MILES_TO_KILOMETERS"})
+	void shouldConvertToHigherValue(ConversionType conversionType) {
+		MeasurementConverter converter = new MeasurementConverter();
+		double value = new Random().nextDouble();
+		double convert = converter.convert(value, conversionType);
+		assertTrue(convert > value);
 	}
 }
