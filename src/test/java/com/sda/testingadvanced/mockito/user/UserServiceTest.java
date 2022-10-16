d69c3d01f16de2d1ef9f5f2b4b7a62328bf04a5e
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2022-10-16 14:54:05 +0200

    - UserServiceTest scratch

---
+++ src/test/java/com/sda/testingadvanced/mockito/user/UserServiceTest.java
@@ -0,0 +1,18 @@
+package com.sda.testingadvanced.mockito.user;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class UserServiceTest {
+
+	@Test
+	void shouldFindUserById() {
+
+	}
+
+	@Test
+	void shouldThrowExceptionWhenUserDoesNotExists() {
+
+	}
+}
