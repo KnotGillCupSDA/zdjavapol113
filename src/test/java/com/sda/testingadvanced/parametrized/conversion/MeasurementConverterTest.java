0256b1376463798e917d01c23fd4ca683909229d
Tomasz Wozniak <tomasz.wozniak@dynatrace.com>
2022-10-16 11:44:43 +0200

    - MeasurementConverter with beforeAll

--- src/test/java/com/sda/testingadvanced/parametrized/conversion/MeasurementConverterTest.java
+++ src/test/java/com/sda/testingadvanced/parametrized/conversion/MeasurementConverterTest.java
@@ -5,15 +5,22 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Random;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
 
 class MeasurementConverterTest {
 
+	private static MeasurementConverter converter;
+
+	@BeforeAll
+	static void beforeAll() {
+		converter = new MeasurementConverter();
+	}
+
 	@ParameterizedTest
 	@EnumSource
 	void shouldConvertToDoubleWithBoundaries(ConversionType conversionType) {
-		MeasurementConverter converter = new MeasurementConverter();
 		double actual = converter.convert(10.0, conversionType);
 
 		assertTrue(actual < Double.MAX_VALUE);
@@ -25,7 +32,6 @@ class MeasurementConverterTest {
 	@EnumSource(value = ConversionType.class,
 	names = {"METERS_TO_YARDS", "INCHES_TO_CENTIMETERS", "MILES_TO_KILOMETERS"})
 	void shouldConvertToHigherValue(ConversionType conversionType) {
-		MeasurementConverter converter = new MeasurementConverter();
 		double value = new Random().nextDouble();
 		double convert = converter.convert(value, conversionType);
 		assertTrue(convert > value);
